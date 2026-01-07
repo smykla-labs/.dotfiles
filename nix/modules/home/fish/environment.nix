2151d94fcf90550959e2d97b5845f485728d90c5
Bart Smykla <bartek@smykla.com>
2026-01-07 12:12:54 +0100

    fix(fish): restore missing color theme settings
    Fish color theme settings (fish_color_* variables) were not migrated
    when dotfiles were moved from chezmoi to nix. This caused abbreviations
    and other syntax elements to appear in white/default color, making them
    indistinguishable from regular text.
    
    Added all fish_color_* and fish_pager_color_* universal variables back
    to environment.nix to restore proper syntax highlighting.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- nix/modules/home/fish/environment.nix
+++ nix/modules/home/fish/environment.nix
@@ -7,6 +7,35 @@
     # Disable initial welcome message
     set --global fish_greeting
 
+    # Fish color theme (syntax highlighting)
+    set --universal fish_color_autosuggestion brblack
+    set --universal fish_color_cancel -r
+    set --universal fish_color_command normal
+    set --universal fish_color_comment red
+    set --universal fish_color_cwd green
+    set --universal fish_color_cwd_root red
+    set --universal fish_color_end green
+    set --universal fish_color_error brred
+    set --universal fish_color_escape brcyan
+    set --universal fish_color_history_current --bold
+    set --universal fish_color_host normal
+    set --universal fish_color_host_remote yellow
+    set --universal fish_color_normal normal
+    set --universal fish_color_operator brcyan
+    set --universal fish_color_param cyan
+    set --universal fish_color_quote yellow
+    set --universal fish_color_redirection cyan --bold
+    set --universal fish_color_search_match white --background=brblack
+    set --universal fish_color_selection white --bold --background=brblack
+    set --universal fish_color_status red
+    set --universal fish_color_user brgreen
+    set --universal fish_color_valid_path --underline
+    set --universal fish_pager_color_completion normal
+    set --universal fish_pager_color_description yellow -i
+    set --universal fish_pager_color_prefix normal --bold --underline
+    set --universal fish_pager_color_progress brwhite --background=cyan
+    set --universal fish_pager_color_selected_background -r
+
     # Environment variables
     set --export PROJECTS_PATH $HOME/Projects/github.com
     set --export MY_PROJECTS_PATH $PROJECTS_PATH/bartsmykla
